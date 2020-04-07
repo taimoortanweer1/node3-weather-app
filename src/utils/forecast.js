0a4bed1fe777e7369a021ad9e40ec4aad27f3d5f
Taimoor Tanweer <taimoor.tanweer1990@gmail.com>
2020-04-07 23:14:33 +0500

    Removing forecast call unneccsara commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 
-//////////////////////
+///////////////////////
 
 const forecast = (lat,long,callback) => {    
     //const url = 'api.openweathermap.org/data/2.5/weather?lat='+encodeURIComponent(lat)+'&lon='+encodeURIComponent(long)+'&appid=69ccd87a0e550d5dc177517e5d4f8df6';
