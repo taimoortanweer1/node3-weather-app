43e761e5e8f276456b8eda838e21b8942fb128bf
Taimoor Tanweer <taimoor.tanweer1990@gmail.com>
2020-04-07 23:10:09 +0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+const app = express();
+
+//define path
+const publicDir = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+//setup handle bar engine and view location
+app.set("views", viewsPath);
+app.set("view engine", "hbs");
+hbs.registerPartials(partialPath);
+
+//setup static directory to serve
+app.use(express.static(publicDir));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Taimoor",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Taimoor",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Taimoor",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "No Address Provided",
+    });
+  } else {
+    geocode(req.query.address, (error, { lattitude, longitude, location } = {} ) => {
+      if (error) {
+        return res.send({
+          error: "No Address Provided",
+        });
+      }
+
+      forecast(lattitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error: "No Address Provided",
+          });
+          }
+    
+        res.send([
+          {
+            forecast: forecastData.temp,
+            location,
+            address: req.query.address,                        
+          },          
+        ]);
+      });
+    });
+  }
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 Error",
+    name: "Taimoor",
+    errorMessage: "Help Page Not Found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404 Error",
+    name: "Taimoor",
+    errorMessage: "Page Not Found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is up and running");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+
+//////////////////////
+
+const forecast = (lat,long,callback) => {    
+    //const url = 'api.openweathermap.org/data/2.5/weather?lat='+encodeURIComponent(lat)+'&lon='+encodeURIComponent(long)+'&appid=69ccd87a0e550d5dc177517e5d4f8df6';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=69ccd87a0e550d5dc177517e5d4f8df6'
+    
+    request({url, json:true},(error,{body}) => {
+       if(error){
+           console.log(error)
+           callback("Unable to connect to Server",undefined)
+       }else if(body.cod === 404){
+           callback("Unable to find weather, try another search ",undefined)
+       }else{
+           callback(undefined,{
+               temp: body.main.temp,                              
+           });
+       }
+    })    
+}
+
+module.exports = forecast;
